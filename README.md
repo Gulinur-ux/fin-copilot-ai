7d48a92e5d90de5984b3409882979934d2ba29e2
Gulinur-ux <ruziyevagulinur@gmail.com>
2026-01-18 13:18:01 +0500

    feat: Initialize App

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# fin-copilot-ai
-moliyaviy hisobot tayyorlaydigan suniy intelekt
+# Fin Copilot AI
+Moliyaviy hisobot tayyorlaydigan sun'iy intellekt (Financial Report Generator Agent)
+
+### Installation
+
+You can install this app using the [bench](https://github.com/frappe/bench) CLI:
+
+```bash
+cd $PATH_TO_YOUR_BENCH
+bench get-app https://github.com/Gulinur-ux/fin-copilot-ai.git
+bench install-app fin_copilot
+```
+
+### Contributing
+
+This app uses `pre-commit` for code formatting and linting. Please [install pre-commit](https://pre-commit.com/#installation) and enable it for this repository:
+
+```bash
+cd apps/fin_copilot
+pre-commit install
+```
+
+Pre-commit is configured to use the following tools for checking and formatting your code:
+
+- ruff
+- eslint
+- prettier
+- pyupgrade
+
+### CI
+
+This app can use GitHub Actions for CI. The following workflows are configured:
+
+- CI: Installs this app and runs unit tests on every push.
+- Linters: Runs [Frappe Semgrep Rules](https://github.com/frappe/semgrep-rules) and [pip-audit](https://pypi.org/project/pip-audit/) on every pull request.
+
+### License
+
+MIT
---
+++ fin_copilot/hooks.py
@@ -0,0 +1,249 @@
+app_name = "fin_copilot"
+app_title = "Fin Copilot"
+app_publisher = "Gulinur R"
+app_description = "financial hisobot yasab beruvchi agent"
+app_email = "ruziyevagulinur@gmail.com"
+app_license = "mit"
+
+# Apps
+# ------------------
+
+# required_apps = []
+
+# Each item in the list will be shown as an app in the apps page
+# add_to_apps_screen = [
+# 	{
+# 		"name": "fin_copilot",
+# 		"logo": "/assets/fin_copilot/logo.png",
+# 		"title": "Fin Copilot",
+# 		"route": "/fin_copilot",
+# 		"has_permission": "fin_copilot.api.permission.has_app_permission"
+# 	}
+# ]
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/fin_copilot/css/fin_copilot.css"
+# app_include_js = "/assets/fin_copilot/js/fin_copilot.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/fin_copilot/css/fin_copilot.css"
+# web_include_js = "/assets/fin_copilot/js/fin_copilot.js"
+
+# include custom scss in every website theme (without file extension ".scss")
+# website_theme_scss = "fin_copilot/public/scss/website"
+
+# include js, css files in header of web form
+# webform_include_js = {"doctype": "public/js/doctype.js"}
+# webform_include_css = {"doctype": "public/css/doctype.css"}
+
+# include js in page
+# page_js = {"page" : "public/js/file.js"}
+
+# include js in doctype views
+# doctype_js = {"doctype" : "public/js/doctype.js"}
+# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+
+# Svg Icons
+# ------------------
+# include app icons in desk
+# app_include_icons = "fin_copilot/public/icons.svg"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+# 	"Role": "home_page"
+# }
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Jinja
+# ----------
+
+# add methods and filters to jinja environment
+# jinja = {
+# 	"methods": "fin_copilot.utils.jinja_methods",
+# 	"filters": "fin_copilot.utils.jinja_filters"
+# }
+
+# Installation
+# ------------
+
+# before_install = "fin_copilot.install.before_install"
+# after_install = "fin_copilot.install.after_install"
+
+# Uninstallation
+# ------------
+
+# before_uninstall = "fin_copilot.uninstall.before_uninstall"
+# after_uninstall = "fin_copilot.uninstall.after_uninstall"
+
+# Integration Setup
+# ------------------
+# To set up dependencies/integrations with other apps
+# Name of the app being installed is passed as an argument
+
+# before_app_install = "fin_copilot.utils.before_app_install"
+# after_app_install = "fin_copilot.utils.after_app_install"
+
+# Integration Cleanup
+# -------------------
+# To clean up dependencies/integrations with other apps
+# Name of the app being uninstalled is passed as an argument
+
+# before_app_uninstall = "fin_copilot.utils.before_app_uninstall"
+# after_app_uninstall = "fin_copilot.utils.after_app_uninstall"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "fin_copilot.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# DocType Class
+# ---------------
+# Override standard doctype classes
+
+# override_doctype_class = {
+# 	"ToDo": "custom_app.overrides.CustomToDo"
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+# 	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"fin_copilot.tasks.all"
+# 	],
+# 	"daily": [
+# 		"fin_copilot.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"fin_copilot.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"fin_copilot.tasks.weekly"
+# 	],
+# 	"monthly": [
+# 		"fin_copilot.tasks.monthly"
+# 	],
+# }
+
+# Testing
+# -------
+
+# before_tests = "fin_copilot.install.before_tests"
+
+# Overriding Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "fin_copilot.event.get_events"
+# }
+#
+# each overriding function accepts a `data` argument;
+# generated from the base implementation of the doctype dashboard,
+# along with any modifications made in other Frappe apps
+# override_doctype_dashboards = {
+# 	"Task": "fin_copilot.task.get_dashboard_data"
+# }
+
+# exempt linked doctypes from being automatically cancelled
+#
+# auto_cancel_exempted_doctypes = ["Auto Repeat"]
+
+# Ignore links to specified DocTypes when deleting documents
+# -----------------------------------------------------------
+
+# ignore_links_on_delete = ["Communication", "ToDo"]
+
+# Request Events
+# ----------------
+# before_request = ["fin_copilot.utils.before_request"]
+# after_request = ["fin_copilot.utils.after_request"]
+
+# Job Events
+# ----------
+# before_job = ["fin_copilot.utils.before_job"]
+# after_job = ["fin_copilot.utils.after_job"]
+
+# User Data Protection
+# --------------------
+
+# user_data_fields = [
+# 	{
+# 		"doctype": "{doctype_1}",
+# 		"filter_by": "{filter_by}",
+# 		"redact_fields": ["{field_1}", "{field_2}"],
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_2}",
+# 		"filter_by": "{filter_by}",
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_3}",
+# 		"strict": False,
+# 	},
+# 	{
+# 		"doctype": "{doctype_4}"
+# 	}
+# ]
+
+# Authentication and authorization
+# --------------------------------
+
+# auth_hooks = [
+# 	"fin_copilot.auth.validate"
+# ]
+
+# Automatically update python controller files with type annotations for this app.
+# export_python_type_annotations = True
+
+# default_log_clearing_doctypes = {
+# 	"Logging DocType Name": 30  # days to retain logs
+# }
+
+# Translation
+# ------------
+# List of apps whose translatable strings should be excluded from this app's translations.
+# ignore_translatable_strings_from = []
+
